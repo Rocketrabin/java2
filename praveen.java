c86b3c318449056296411fbe9cd5e3602dd06a17
Rocketrabin <47375817+Rocketrabin@users.noreply.github.com>
2019-02-06 01:20:24 -0800

    Create praveen.java

---
+++ praveen.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int x;
+	Scanner n=new Scanner(System.in);
+	x=n.nextInt();
+	
+		if(x%2==0)
+		{System.out.println("even");
+		}
+		else
+		{System.out.println("odd");} 
+		}
+}
